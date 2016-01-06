112b1394257de78ea629c76f44a4a8bb54ff076f
David Taieb <david_taieb@us.ibm.com>
2016-01-05 21:42:27 -0500

    Update

--- lib/python/flightPredict/setup.py
+++ lib/python/flightPredict/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='flightPredict',
       version='0.2',
       description='Python Library for SPark MLLIB Flight Predict sample application',
-      url='',
+      url='https://github.com/ibm-cds-labs/pipes-connector-flightstats.git',
       author='David Taieb',
       author_email='david_taieb@us.ibm.com',
       license='Apache 2.0',
